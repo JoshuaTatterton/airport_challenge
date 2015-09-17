af135b86872f3bf25a6c2816a0d3262beacdf4e0
Josh <Tattsy@hotmail.co.uk>
2015-09-17 14:11:43 +0100

    refactored and can now use custom capacities

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,59 +2,46 @@ require 'airport'
 
 describe Airport do
 
-  let(:plane){double :plane}
+  let(:plane) { double :plane, land: true, take_off: true }
 
+  before(:each) { allow(subject).to receive(:set_weather) { :Sunny } }
   describe 'ariving' do
     it 'instructs a plane to land' do
-      expect(subject).to respond_to :ariving
+      expect(subject).to respond_to :land_plane
     end
 
     it 'receives a plane' do
-      allow(subject).to receive(:set_weather){"Sunny"}
-      allow(plane).to receive(:land)
-
-      subject.ariving(plane)
-      expect(subject.planes).to_not eq []
+      subject.land_plane(plane)
+      expect(subject.planes).to_not be_empty
     end
 
     it "plane should land on arival" do
-      allow(subject).to receive(:set_weather){"Sunny"}
-
       plane = spy :plane
-      subject.ariving(plane)
+      subject.land_plane(plane)
       expect(plane).to have_received(:land)
-      # This is only testing if it recieves method .land the outcome doesn't matter as that is a problem for the plane spec.
     end
   end
 
   describe 'leaving' do
     it 'instructs a plane to take off' do
-      expect(subject).to respond_to :leaving
+      expect(subject).to respond_to :leaving_plane
     end
 
     it 'releases a plane' do
-      allow(subject).to receive(:set_weather){"Sunny"}
-      allow(plane).to receive(:land)
-      allow(plane).to receive(:take_off)
-
-      subject.ariving(plane)
-      subject.leaving(plane)
-      expect(subject.planes).to eq []
+      subject.land_plane(plane)
+      subject.leaving_plane(plane)
+      expect(subject.planes).to be_empty
     end
 
     it "plane should take off on leaving" do
-      allow(subject).to receive(:set_weather){"Sunny"}
-
       plane = spy :plane
-      subject.ariving(plane)
-      subject.leaving(plane)
+      subject.land_plane(plane)
+      subject.leaving_plane(plane)
       expect(plane).to have_received(:take_off)
-      # This is the same as testing the .land, outcome doesn't matter.
     end
 
     it "shouldn't let out planes that aren't in the airport" do
-      allow(subject).to receive(:set_weather){"Sunny"}
-      expect{subject.leaving(plane)}.to raise_error "The plane is not at this airport"
+      expect { subject.leaving_plane(plane) }.to raise_error "The plane is not at this airport"
     end
 
   end
@@ -62,32 +49,35 @@ describe Airport do
   context 'traffic control' do
     context 'when airport is full' do
       it 'does not allow a plane to land' do
-        allow(subject).to receive(:set_weather){"Sunny"}
-        allow(plane).to receive(:land)
-
-        subject.capacity.times {subject.ariving(plane)}
-        expect{subject.ariving(plane)}.to raise_error "Cannot land, airport full"
+        subject.capacity.times {subject.land_plane(plane)}
+        expect { subject.land_plane(plane) }.to raise_error "Cannot land, airport full"
       end 
     end
 
     context 'when weather conditions are stormy' do
-      
       it 'does not allow a plane to take off' do
-        allow(subject).to receive(:set_weather){"Sunny"}
-        allow(plane).to receive(:land)
-
-        subject.ariving(plane)
-
-        allow(subject).to receive(:set_weather){"Stormy"}
-
-        expect{subject.leaving(plane)}.to raise_error "Cannot take off, weather conditions too bad"
+        subject.land_plane(plane)
+        allow(subject).to receive(:set_weather) { :Stormy }
+        expect { subject.leaving_plane(plane) }.to raise_error "Cannot take off, weather conditions too bad"
       end
 
       it 'does not allow a plane to land' do
-        allow(subject).to receive(:set_weather){"Stormy"}
-
-        expect{subject.ariving(plane)}.to raise_error "Cannot land, weather conditions too bad"
+        allow(subject).to receive(:set_weather) { :Stormy }
+        expect { subject.land_plane(plane) }.to raise_error "Cannot land, weather conditions too bad"
       end 
+    end
+  end
+  context 'capacity' do
+    it 'is defaulted to 10 planes' do
+      expect(subject.capacity).to eq 10
+    end
+    it 'can be custom on creation' do
+      airport = Airport.new(11)
+      expect(airport.capacity).to eq 11
+    end
+    it 'can be changed after creation' do
+      subject.capacity = 5
+      expect(subject.capacity).to eq 5
     end
   end
 end
